b4d6187010889e3335c2b0314c0d5497baf6e668
gajuu <lizzgaju@gmail.com>
2026-04-07 23:50:25 +0100

    updated intake,user model and also intake,user routes

--- backend/model/intakesModel.js
+++ backend/model/intakesModel.js
@@ -1,27 +1,39 @@
 import sequelize from "../config/db.js";
 import { DataTypes } from "sequelize";
-const intake= sequelize.define("Intake",{
-    intake_id:{
-        type:DataTypes.INTEGER,
-        allowNull:false,
-        autoIncrement:true,
-        primaryKey:true
-    },
-    intake_name:{
-        type:DataTypes.STRING,
-        allowNull:false,
 
+const Intake = sequelize.define("Intake", {
+    intake_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
     },
-    start_date:{
-        type:DataTypes.DATE,
-        allowNull:false,
-   
+    intake_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        }
     },
-    end_date:{
-        type:DataTypes.DATE,
-        allowNull:false,
+    start_date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    end_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        validate: {
+            isAfterStart(value) {
+                if (value <= this.start_date) {
+                    throw new Error("End date must be after start date");
+                }
+            }
+        }
+    }
+}, {
+    tableName: "intake",
+    timestamps: true,
+    freezeTableName: true
+});
 
-    },
-},{tableName:"intake", timestamps:true})
-
-export default intake;
+export default Intake;
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -1,22 +1,43 @@
 import sequelize from "../config/db.js";
 import { DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
 
-const user= sequelize.define("User",{
-    user_id:{
-        type:DataTypes.INTEGER,
-        allowNull:false,
-        primaryKey:true,
-        autoIncrement:true,
+const User = sequelize.define("User", {
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
     },
-    username:{
-        type:DataTypes.STRING,
-        allowNull:false,
-      
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            notEmpty: true,
+            len: [3, 50]
+        }
     },
-    password:{
-        type:DataTypes.STRING,
-        allowNull:false,
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
     },
-},{tableName:"user",timestamps:false})
+}, {
+    tableName: "user",
+    timestamps: false,
+    hooks: {
+        beforeCreate: async (user) => {
+            user.password = await bcrypt.hash(user.password, 10);
+        },
+        beforeUpdate: async (user) => {
+            if (user.changed("password")) {
+                user.password = await bcrypt.hash(user.password, 10);
+            }
+        }
+    },
+    defaultScope: {
+        attributes: { exclude: ["password"] }
+    }
+});
 
-export default user;
+export default User;
